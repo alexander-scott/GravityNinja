9d063c8022bd3814ec0f0bbfa9a7eb13db5fcaed
Alisdair101 <AlisdairTW@gmail.com>
2015-02-03 18:08:07 +0000

    Revert "[-] Duck Class Deleted"
    This reverts commit b331e737e4fcde66479bb67d3eed4633bb2d19d6.

---
+++ GravityDuck/GravityDuck/Duck.cs
@@ -0,0 +1,20 @@
+using System;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Graphics;
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+using Sce.PlayStation.Core.Input;
+using Sce.PlayStation.Core.Audio;
+
+namespace GravityDuck
+{
+	public class Duck
+	{
+		public Duck ()
+		{
+		}
+	}
+}
+
