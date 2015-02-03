6bfa93aee24fb656ec3ba29d60341c263b0423d0
Alisdair101 <AlisdairTW@gmail.com>
2015-02-03 20:13:46 +0000

    [+] Maze Class Updated [AW]
    [+] Basic Level Created to test new features
    [+] New tile types added to system

---
+++ GravityDuck/GravityDuck/Maze.cs
@@ -0,0 +1,124 @@
+using System;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Graphics;
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+
+namespace GravityDuck
+{
+	//Our Maze class V1.0 by @AS
+	public class Maze
+	{
+		private TextureInfo groundBlock1;
+		private TextureInfo groundBlock2;
+		
+		private TextureInfo ceilingBlock1;
+		private TextureInfo ceilingBlock2;
+		
+		private TextureInfo treeBlock;
+		
+		private SpriteUV[,] 	sprites; //Each block is a sprite
+		private int mazeWidth = 8, mazeHeight = 11; //Width and height for the maze
+						
+		public Maze (Scene scene)
+		{
+			//Load in the textures here
+			//Ground Block Textures
+			groundBlock1 = new TextureInfo("/Application/textures/Level/groundBlock1.png");
+			groundBlock2 = new TextureInfo("/Application/textures/Level/groundBlock2.png");
+			
+			//Ceiling Block Textures
+			ceilingBlock1 = new TextureInfo("/Application/textures/Level/ceilingBlock1.png");
+			ceilingBlock2 = new TextureInfo("/Application/textures/Level/ceilingBlock2.png");
+			
+			treeBlock = new TextureInfo("/Application/textures/Level/treeBlock.png");
+			
+			sprites	= new SpriteUV[mazeWidth,mazeHeight]; //Initalise the sprites
+						
+			int[,] tileMap ={ {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, //Basic layout for a map
+						      {5, 1, 0, 3, 1, 0, 0, 0, 0, 3, 5}, //Slight bug:
+							  {5, 2, 0, 4, 2, 0, 3, 1, 0, 4, 5}, //For some reason the layout is rotated
+							  {5, 1, 0, 3, 1, 0, 4, 2, 0, 3, 5}, //90 degrees to the left when drawn
+							  {5, 2, 0, 4, 2, 0, 3, 1, 0, 4, 5}, //so when creating the array this needs
+							  {5, 1, 0, 3, 1, 0, 4, 2, 0, 3, 5}, //to be taken into account.
+							  {5, 2, 0, 0, 0, 0, 3, 1, 0, 4, 5}, //I'll fix it later
+							  {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5} };
+				
+			for (int i = 0; i < mazeWidth; ++i) //Basic tile engine
+			{
+    			for (int j = 0; j < mazeHeight; ++j) //Goes through every number/coordinate in the array
+				{
+					if (tileMap[i,j] == 1) //If the tile has an ID of 1 it's a rigid block
+					{
+						sprites[i,j] 		  = new SpriteUV(groundBlock1);
+						sprites[i,j].Quad.S   = groundBlock1.TextureSizef;
+						sprites[i,j].Position = new Vector2(128.0f*i, 142.0f*j); //Place them at intervals
+						scene.AddChild(sprites[i,j]);
+					}
+					else if(tileMap[i,j] == 2) //Else place the block off-screen
+					{
+						sprites[i,j] 		  = new SpriteUV(groundBlock2);
+						sprites[i,j].Quad.S   = groundBlock2.TextureSizef;
+						sprites[i,j].Position = new Vector2(128.0f*i, 142.0f*j); //Place them at intervals
+						scene.AddChild(sprites[i,j]);
+					}
+					else if(tileMap[i,j] == 3) //Else place the block off-screen
+					{
+						sprites[i,j] 		  = new SpriteUV(ceilingBlock1);
+						sprites[i,j].Quad.S   = ceilingBlock1.TextureSizef;
+						sprites[i,j].Position = new Vector2(128.0f*i, 142.0f*j); //Place them at intervals
+						scene.AddChild(sprites[i,j]);
+					}
+					else if(tileMap[i,j] == 4) //Else place the block off-screen
+					{
+						sprites[i,j] 		  = new SpriteUV(ceilingBlock2);
+						sprites[i,j].Quad.S   = ceilingBlock2.TextureSizef;
+						sprites[i,j].Position = new Vector2(128.0f*i, 142.0f*j); //Place them at intervals
+						scene.AddChild(sprites[i,j]);
+					}
+					else if(tileMap[i,j] == 5) //Else place the block off-screen
+					{
+						sprites[i,j] 		  = new SpriteUV(treeBlock);
+						sprites[i,j].Quad.S   = treeBlock.TextureSizef;
+						sprites[i,j].Position = new Vector2(128.0f*i, 142.0f*j); //Place them at intervals
+						scene.AddChild(sprites[i,j]);
+					}
+					else
+					{
+						sprites[i,j] = new SpriteUV(null);
+					}
+				}
+			}
+		}
+		
+		public void Dispose() //Dispose texture data
+		{
+			groundBlock1.Dispose();
+			groundBlock2.Dispose();
+			ceilingBlock1.Dispose();
+			ceilingBlock2.Dispose();
+			treeBlock.Dispose();
+		}
+		
+		public bool HasCollidedWithPlayer(SpriteUV sprite) //Check if the a sprite has hit a part of the maze
+		{
+			Bounds2 player = sprite.GetlContentLocalBounds();
+			sprite.GetContentWorldBounds(ref player ); //Get sprite bounds (player bounds)
+			
+			foreach(SpriteUV spri in sprites)
+			{
+				Bounds2 mazeTile = sprite.GetlContentLocalBounds();
+				spri.GetContentWorldBounds(ref mazeTile); //Get all of the maze bounds
+				
+				if (mazeTile.Overlaps(player)) //Return true if the player overlaps with the maze
+				{
+				   return true;
+				}
+			}
+			return false;
+		}
+	}
+}
+
---
+++ README.txt
@@ -0,0 +1,33 @@
+Danger Ducks #SWAG
+
+Concept:
+Simple 2D platformer where you are unable to control the player but are able to manipulate the world around him including being able to rotate, 
+flip, change gravity the level he is on. The game will be played as a side scroller where you must navigate through a maze type map with all 
+kinds of obstacles/puzzles/enemies. The main point of this game is to use/abuse physics to solve problems to complete the maze. The player 
+will be constantly moving in one direction and you will be around to rotate the map with the touchscreen (or whatever) to avoid obstacles 
+and get to your destination.
+ 
+How it will be made:
+The maze itself will be made from hundreds of rigid, non-moveable textured blocks. The method to create the maze from these blocks will be to 
+read in from a text file containing a 100x100 (or bigger/smaller) map of numbers with each number representing either an empty square or a block. 
+This means it looks more professional as it’s not hard-coded and it is easier to change the design of the maze or add more maze levels or add 
+another block entity. The rotation feature will be done so the map doesn’t rotate but the camera does, giving the illusion that the map is 
+rotating. Alongside this the gravity vector that the player has will need to change as the camera does, as does the direction vector the 
+player is moving in. 
+
+If the slope that the player is moving up is too steep the player will move much slower or will eventually travel backwards. 
+Again if the slope of the maze is too shallow the player will move quicker down it. PHYSICS. Changing gravity is simple as you just invert the 
+gravity vector. The player will have a speed float which can only be affected by the slopes of the maze (steeper = slower) and the players 
+velocity. The player will have a default direction vector which will change a lot as the maze gets rotated. If that vector gets too steep it 
+will trigger a bool that will invert it making the player travel in the opposite direction. The player will have a velocity/momentum float that 
+makes the player fall and move more realistically. If the player is falling and lands on his head he will need to rotate so his feet are on 
+
+the surface before he starts moving again (unless of course the user decides to make him fall again).The player will be a simple textured sprite and 
+possibly animated. The blocks will be textured as grass towards the beginning of the maze at 0,0 and turn darker as the player progresses through. 
+Behind the maze there will be a texture which again will be light and happy towards the beginning (probably clouds or something) and turn darker 
+as you progress (eventually becomes hell). As for the HUD on the screen there will most likely be time left and distance that the player has done. 
+There will also be a cool start and death menu, with the death menu displaying current score which will be calculated by __________ and high scores 
+which will be read from file. 
+
+Implementing smooth touch screen usage does seem quite hard but should be fine. All code should be commented pls and good variable names and stuff. 
+We can work on the design of each part of the level or indiviual levels later.
